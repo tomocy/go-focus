f52200c351401d0d15f236203bdd20959f9e8732
tomocy <tomocy.dev@gmail.com>
2020-04-12 20:50:01 +0900

    update SessionRepo
    SessionRepo can delete sessions.

--- session.go
+++ session.go
@@ -5,6 +5,7 @@ import "context"
 type SessionRepo interface {
 	Pull(context.Context) (*Session, error)
 	Push(context.Context, *Session) error
+	Delete(context.Context) error
 }
 
 type Session struct {
