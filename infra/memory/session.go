fe03fd464e94cc19ce87fa00591bcc431cd8dd1b
tomocy <tomocy.dev@gmail.com>
2020-04-12 20:54:31 +0900

    update memory.sessionRepo
    SessionRepo can pull sessions.

--- infra/memory/session.go
+++ infra/memory/session.go
@@ -1,7 +1,19 @@
 package memory
 
-import "github.com/tomocy/focus"
+import (
+	"context"
+
+	"github.com/tomocy/focus"
+)
 
 type sessionRepo struct {
 	sess *focus.Session
+}
+
+func (r sessionRepo) Pull(context.Context) (*focus.Session, error) {
+	if r.sess == nil {
+		return nil, focus.ErrNoSession
+	}
+
+	return r.sess, nil
 }
