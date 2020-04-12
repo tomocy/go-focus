5b1591c4b4a8c088dcc238557418a57c4d8e0b9e
tomocy <tomocy.dev@gmail.com>
2020-04-12 20:34:58 +0900

    update memory.userRepo
    UserRepo can generate user ids.

--- infra/memory/memory.go
+++ infra/memory/memory.go
@@ -1,7 +1,16 @@
 package memory
 
-import "github.com/tomocy/focus"
+import (
+	"context"
+
+	"github.com/tomocy/focus"
+	"github.com/tomocy/focus/infra/rand"
+)
 
 type userRepo struct {
 	users map[focus.UserID]*focus.User
+}
+
+func (r userRepo) NextID(context.Context) (focus.UserID, error) {
+	return focus.UserID(rand.GenerateString(30)), nil
 }
