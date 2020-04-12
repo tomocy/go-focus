63095a6dcc018979e1bbf9c5fd9c88ea8cb2aba2
tomocy <tomocy.dev@gmail.com>
2020-04-12 21:27:58 +0900

    update registerUser
    RegisterUser can register users.

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -1,8 +1,45 @@
 package usecase
 
-import "github.com/tomocy/focus"
+import (
+	"context"
+	"fmt"
+
+	"github.com/tomocy/focus"
+)
 
 type registerUser struct {
 	userRepo focus.UserRepo
 	sessRepo focus.SessionRepo
+}
+
+func (u *registerUser) Do(email, pass string) (*focus.User, error) {
+	ctx := context.TODO()
+
+	id, err := u.userRepo.NextID(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate user id: %w", err)
+	}
+	hashed, err := focus.HashPassword(pass)
+	if err != nil {
+		return nil, fmt.Errorf("failed to hash password: %w", err)
+	}
+	user, err := focus.NewUser(id, email, hashed)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := u.userRepo.Save(ctx, user); err != nil {
+		return nil, fmt.Errorf("failed to save user: %s", err)
+	}
+
+	sess, err := focus.NewSession(user.ID())
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate session: %w", err)
+	}
+
+	if err := u.sessRepo.Push(ctx, sess); err != nil {
+		return nil, fmt.Errorf("failed to push session: %w", err)
+	}
+
+	return user, nil
 }
--- usecase/usecase_test.go
+++ usecase/usecase_test.go
@@ -1,8 +1,97 @@
 package usecase
 
 import (
+	"context"
 	"fmt"
+	"testing"
+
+	"github.com/tomocy/focus"
+	"github.com/tomocy/focus/infra/memory"
 )
+
+func TestRegisterUser(t *testing.T) {
+	userRepo, sessRepo := memory.NewUserRepo(), memory.NewSessionRepo()
+
+	u := registerUser{
+		userRepo: userRepo,
+		sessRepo: sessRepo,
+	}
+
+	email, pass := "email", "pass"
+	user, err := u.Do(email, pass)
+	if err != nil {
+		t.Errorf("should have registered user: %s", err)
+		return
+	}
+
+	if err := assertUser(
+		user,
+		checkIfIDOfUserIsFilled(), checkIfEmailOfUserIsSame(email), checkIfPasswordOfUserIsCorrect(pass),
+	); err != nil {
+		t.Errorf("should have returned the reigstered user: %s", err)
+		return
+	}
+
+	saved, err := userRepo.FindByEmail(context.Background(), email)
+	if err != nil {
+		t.Errorf("should have saved the registered user: %s", err)
+		return
+	}
+	if err := assertUser(
+		saved,
+		checkIfIDOfUserIsFilled(), checkIfEmailOfUserIsSame(email), checkIfPasswordOfUserIsCorrect(pass),
+	); err != nil {
+		t.Errorf("should have returned the reigstered user: %s", err)
+		return
+	}
+
+	if _, err := sessRepo.Pull(context.Background()); err != nil {
+		t.Errorf("should have push session of the registerd user: %s", err)
+		return
+	}
+}
+
+func assertUser(u *focus.User, ops ...assertUserOption) error {
+	for _, o := range ops {
+		if err := o(u); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type assertUserOption func(u *focus.User) error
+
+func checkIfIDOfUserIsFilled() assertUserOption {
+	return func(u *focus.User) error {
+		if u.ID() == "" {
+			return fmt.Errorf("empty id")
+		}
+
+		return nil
+	}
+}
+
+func checkIfEmailOfUserIsSame(email string) assertUserOption {
+	return func(u *focus.User) error {
+		if u.Email() != email {
+			return reportUnexpected("email", u.Email(), email)
+		}
+
+		return nil
+	}
+}
+
+func checkIfPasswordOfUserIsCorrect(plain string) assertUserOption {
+	return func(u *focus.User) error {
+		if !u.Password().IsSame(plain) {
+			return fmt.Errorf("incorrect password")
+		}
+
+		return nil
+	}
+}
 
 func reportUnexpected(name string, actual, expected interface{}) error {
 	return fmt.Errorf("unexpected %s: got %v, expected %v", name, actual, expected)
